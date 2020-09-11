1224bbffa9f598fce5af80aa9f46da24a1bafbbb
sof1111 <student@C320U24>
2020-09-11 11:11:30 +0300

    prt02

---
+++ practic1/01/2/prt02/prt02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace prt02
+{
+  class Program
+  {
+    struct Person
+    {
+      public enum Genders : int { Male, Female };
+
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Genders gender;
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
+      {
+        firstName = _firstName;
+        lastName = _lastName;
+        age = _age;
+        gender = _gender;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + " (" + gender + "), age " + age;
+      }
+    }
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", Person.Genders.Male);
+      Console.WriteLine(p.ToString());
+    }
+  }
+}
