314c856c4a98f7e4eb409fc0cf23a213fb5f574d
sof1111 <student@C320U24>
2020-09-11 10:24:13 +0300

    fix folders structure

---
+++ prt01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace prt01
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+
+      public Person(string _firstName, string _lastName, int _age)
+      {
+        firstName = _firstName;
+        lastName = _lastName;
+        age = _age;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + ", age " + age;
+      }
+    }
+       
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32); 
+      Console.WriteLine(p);
+    }
+  }
+}
