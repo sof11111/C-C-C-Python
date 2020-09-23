9dafe474356927e07570f2085c63237d5608e9e7
Соф <sofiya-goel@mail.ru>
2020-09-24 00:27:44 +0300

    prt04

---
+++ practic1/02/2/SortString/SortString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] sa = s.Split(' ');
+
+            Array.Sort(sa);
+
+            s = string.Join(" ", sa);
+            Console.WriteLine(s);
+        }
+    }
+}
