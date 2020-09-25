e018c565905e91f0782fdc9ad06909d5b68d1aa2
sof <s>
2020-09-25 11:26:40 +0300

    prt

---
+++ practic4/1/BasicCollection/BasicCollection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace BasicCollection
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      ArrayList myList = new ArrayList() { "Firs" };
+      myList.Add("Second");
+      myList.Add("Third");
+      myList.Add("Fourth");
+
+      foreach (string item in myList)
+      {
+        Console.WriteLine("Unsorted: {0}", item);
+      }
+      // Сортировка при помощи стандартного объекта сравнения 
+      myList.Sort();
+      //myList.Reverse();
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+
+    }
+
+  }
+}
---
+++ practic4/2/SequentialCollections/SequentialCollections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace SequentialCollections
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Queue queue = new Queue();
+
+      queue.Enqueue("First"); 
+      queue.Enqueue("Second"); 
+      queue.Enqueue("Third"); 
+      queue.Enqueue("Fourth");
+
+      while (queue.Count > 0)
+        {
+          object obj = queue.Dequeue();
+          Console.WriteLine("From Queue: {0}", obj);
+        }
+     }
+  }
+}
+
