b690ea2c3a643a3c29aa9f8fcc6fcf4013ce8255
student <student@C383U18>
2020-10-23 11:25:41 +0300

    prt8

---
+++ practic7/1/prt7_1/prt7_1/Form1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prt7_1
+{
+  class Form1
+  {
+  }
+}
---
+++ practic7/1/prt7_1/prt7_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prt7_1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
