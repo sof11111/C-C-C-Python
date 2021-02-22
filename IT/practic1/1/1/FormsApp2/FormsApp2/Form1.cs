8afd60a5fd0da8bbb29fc32efdcdfea29b3819d6
student <student@C246U26>
2021-02-22 14:42:50 +0300

    prt

---
+++ IT/practic1/1/1/FormsApp2/FormsApp2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FormsApp2
+{
+  public partial class Form1 : Form
+  {
+    List<Button> buttons;
+    public Form1()
+    {
+      InitializeComponent();
+      buttons = new List<Button>();
+      buttons.AddRange(new Button[5] { button1, button2, button3, button4, button5 });
+      button1.Click += button1_Click;
+      button2.Click += button1_Click;
+      button3.Click += button1_Click;
+      button4.Click += button1_Click;
+      button5.Click += button1_Click;
+    }
+
+    void button1_Click(object sender, EventArgs e)
+    {
+      Random rnd = new Random(DateTime.Now.Millisecond);
+      Button btn = buttons[rnd.Next(buttons.Count)];
+      btn.Visible = !btn.Visible;
+    }
+  }
+}
