5f244c0f883d0913219610b31ded812987a16376
student <student@C246U26>
2021-02-22 14:26:44 +0300

    gfd

---
+++ IT/practic1/1/1/zadanie1/zadanie1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace zadanie1
+{
+    private void Form(object sender, EventArgs e)
+    {
+      Random random = new Random();
+      this.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+    }
+    private void Form1_Click(object sender, EventArgs e)
+    {
+      Random random = new Random();
+      this.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+      label1.Text = "Начало работы";
+      textBox1.Text = "";
+    }
+
+
+  }
+}
