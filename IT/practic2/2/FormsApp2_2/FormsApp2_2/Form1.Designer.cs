dc52bb0abc1c9f85b0f3c127c9ea9dcd13ebdf09
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-03-19 22:18:28 +0300

    зке

--- IT/practic2/2/FormsApp2_2/FormsApp2_2/Form1.Designer.cs
+++ IT/practic2/2/FormsApp2_2/FormsApp2_2/Form1.Designer.cs
@@ -33,12 +33,16 @@ namespace FormsApp2_2
       this.textBox1 = new System.Windows.Forms.TextBox();
       this.textBox2 = new System.Windows.Forms.TextBox();
       this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.button1 = new System.Windows.Forms.Button();
+      this.groupBox1.SuspendLayout();
       this.SuspendLayout();
       // 
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(104, 96);
+      this.label1.Location = new System.Drawing.Point(52, 96);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(87, 17);
       this.label1.TabIndex = 0;
@@ -46,23 +50,23 @@ namespace FormsApp2_2
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(197, 96);
+      this.textBox1.Location = new System.Drawing.Point(145, 96);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(100, 22);
       this.textBox1.TabIndex = 1;
       // 
       // textBox2
       // 
-      this.textBox2.Location = new System.Drawing.Point(107, 196);
+      this.textBox2.Location = new System.Drawing.Point(55, 145);
       this.textBox2.Multiline = true;
       this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(260, 143);
+      this.textBox2.Size = new System.Drawing.Size(346, 228);
       this.textBox2.TabIndex = 2;
       // 
       // radioButton1
       // 
       this.radioButton1.AutoSize = true;
-      this.radioButton1.Location = new System.Drawing.Point(588, 97);
+      this.radioButton1.Location = new System.Drawing.Point(6, 30);
       this.radioButton1.Name = "radioButton1";
       this.radioButton1.Size = new System.Drawing.Size(107, 21);
       this.radioButton1.TabIndex = 3;
@@ -70,17 +74,51 @@ namespace FormsApp2_2
       this.radioButton1.Text = "Сумма ряда";
       this.radioButton1.UseVisualStyleBackColor = true;
       // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.radioButton2);
+      this.groupBox1.Controls.Add(this.radioButton1);
+      this.groupBox1.Location = new System.Drawing.Point(431, 96);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(175, 100);
+      this.groupBox1.TabIndex = 4;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Выбор";
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(6, 57);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(161, 21);
+      this.radioButton2.TabIndex = 4;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "Произведение ряда";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(469, 377);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 33);
+      this.button1.TabIndex = 5;
+      this.button1.Text = "Расчет";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Controls.Add(this.radioButton1);
+      this.ClientSize = new System.Drawing.Size(680, 450);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.groupBox1);
       this.Controls.Add(this.textBox2);
       this.Controls.Add(this.textBox1);
       this.Controls.Add(this.label1);
       this.Name = "Form1";
-      this.Text = "Form1";
+      this.Text = "Алгоритм с использованием циклов";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -92,6 +130,9 @@ namespace FormsApp2_2
     private System.Windows.Forms.TextBox textBox1;
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.RadioButton radioButton1;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.RadioButton radioButton2;
+    private System.Windows.Forms.Button button1;
   }
 }
 
