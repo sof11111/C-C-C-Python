839fbe12e6de014af2b1beb108d993e1bc4ee792
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-03-19 15:48:00 +0300

    пр

---
+++ IT/practic2/2/FormsApp2_2/FormsApp2_2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace FormsApp2_2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(104, 96);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(87, 17);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Ввод числа:";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(197, 96);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 22);
+      this.textBox1.TabIndex = 1;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(107, 196);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(260, 143);
+      this.textBox2.TabIndex = 2;
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Location = new System.Drawing.Point(588, 97);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(107, 21);
+      this.radioButton1.TabIndex = 3;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "Сумма ряда";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.radioButton1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.RadioButton radioButton1;
+  }
+}
+
