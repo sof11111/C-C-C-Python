99329904584909dc8afbdf518710dd41beeb11d3
student <student@C239U28>
2021-03-22 09:26:53 +0300

    prt

---
+++ IT/practic3/1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Items.AddRange(new object[] {
+            "Привет Мир ",
+            "Сегодня прекрасный день ",
+            "Изучаем редактор коллекции строк"});
+      this.listBox1.Location = new System.Drawing.Point(59, 109);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(299, 212);
+      this.listBox1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(59, 373);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Пуск";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(245, 378);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 13);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "label1";
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(460, 450);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.listBox1);
+      this.Name = "Form1";
+      this.Text = "Работа со строками";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
