f704d2ede0fc7355e88a844d8b5f3f41ae19183c
student <student@C239U28>
2021-03-22 10:36:56 +0300

    prrt

---
+++ IT/practic3/1/formStroki/formStroki/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formStroki
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+          // Получаем номер выделенной строки
+          int index = listBox1.SelectedIndex;
+
+          // Считываемстрокувперменнуюstr
+          string str = (string)listBox1.Items[index];
+          // Узнаем количество символов в строке
+          int len = str.Length;
+          // Считаем, что количество пробелов равно 0
+          int count = 0;
+          // Устанавливаем счетчик символов в 0
+          //int i = 0;
+
+      //Организуем цикл перебора всех символов в строке
+      /*while (i < len - 1)
+      {
+        // Если нашли пробел, то увеличиваем
+        // счетчик пробелов на 1
+        if (str[i] == ' ')
+          count++;
+        i++;
+      }*/
+
+      string s = (string)listBox1.Items[index];
+      char[] array = s.ToCharArray();
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (array[i] == 'a')
+        {
+          array[i] = 'o';
+        }
+        //Console.Write(array[i]);
+      }
+
+      label1.Text = "Замена = " + array;
+        }
+
+
+  }
+}
