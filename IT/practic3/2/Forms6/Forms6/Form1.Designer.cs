701996f5a2d75c075a3c6542c9227892c81e2d57
student <student@C239U28>
2021-03-22 11:50:22 +0300

    prtt

---
+++ IT/practic3/2/Forms6/Forms6/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Forms6
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(81, 54);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(99, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Исходный массив";
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(365, 54);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(142, 13);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "Преобразованный массив";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(84, 350);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "Заполнить";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(395, 350);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 3;
+      this.button2.Text = "Рассчитать";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(40, 88);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBox1.Size = new System.Drawing.Size(200, 219);
+      this.textBox1.TabIndex = 4;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(339, 88);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBox2.Size = new System.Drawing.Size(188, 219);
+      this.textBox2.TabIndex = 5;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(605, 428);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Одномерные массивы";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
