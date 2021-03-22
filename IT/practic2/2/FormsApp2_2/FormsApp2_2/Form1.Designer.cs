eac356ff31887f911844a2ed7155ac7ce693614f
student <student@C246U24>
2021-03-22 14:09:47 +0300

    prt

--- IT/practic2/2/FormsApp2_2/FormsApp2_2/Form1.Designer.cs
+++ IT/practic2/2/FormsApp2_2/FormsApp2_2/Form1.Designer.cs
@@ -42,34 +42,39 @@ namespace FormsApp2_2
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(52, 96);
+      this.label1.Location = new System.Drawing.Point(39, 78);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(87, 17);
+      this.label1.Size = new System.Drawing.Size(67, 13);
       this.label1.TabIndex = 0;
       this.label1.Text = "Ввод числа:";
       this.label1.Click += new System.EventHandler(this.label1_Click);
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(145, 96);
+      this.textBox1.Location = new System.Drawing.Point(109, 78);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(100, 22);
+      this.textBox1.Size = new System.Drawing.Size(76, 20);
       this.textBox1.TabIndex = 1;
       // 
       // textBox2
       // 
-      this.textBox2.Location = new System.Drawing.Point(55, 145);
+      this.textBox2.Location = new System.Drawing.Point(41, 118);
+      this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.textBox2.Multiline = true;
       this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(346, 228);
+      this.textBox2.Size = new System.Drawing.Size(260, 186);
       this.textBox2.TabIndex = 2;
+      this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
       // 
       // radioButton1
       // 
       this.radioButton1.AutoSize = true;
-      this.radioButton1.Location = new System.Drawing.Point(6, 30);
+      this.radioButton1.Location = new System.Drawing.Point(4, 24);
+      this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.radioButton1.Name = "radioButton1";
-      this.radioButton1.Size = new System.Drawing.Size(107, 21);
+      this.radioButton1.Size = new System.Drawing.Size(86, 17);
       this.radioButton1.TabIndex = 3;
       this.radioButton1.TabStop = true;
       this.radioButton1.Text = "Сумма ряда";
@@ -79,9 +84,11 @@ namespace FormsApp2_2
       // 
       this.groupBox1.Controls.Add(this.radioButton2);
       this.groupBox1.Controls.Add(this.radioButton1);
-      this.groupBox1.Location = new System.Drawing.Point(431, 96);
+      this.groupBox1.Location = new System.Drawing.Point(323, 78);
+      this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(175, 100);
+      this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.groupBox1.Size = new System.Drawing.Size(131, 81);
       this.groupBox1.TabIndex = 4;
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "Выбор";
@@ -89,9 +96,10 @@ namespace FormsApp2_2
       // radioButton2
       // 
       this.radioButton2.AutoSize = true;
-      this.radioButton2.Location = new System.Drawing.Point(6, 57);
+      this.radioButton2.Location = new System.Drawing.Point(4, 46);
+      this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.radioButton2.Name = "radioButton2";
-      this.radioButton2.Size = new System.Drawing.Size(161, 21);
+      this.radioButton2.Size = new System.Drawing.Size(126, 17);
       this.radioButton2.TabIndex = 4;
       this.radioButton2.TabStop = true;
       this.radioButton2.Text = "Произведение ряда";
@@ -99,23 +107,26 @@ namespace FormsApp2_2
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(469, 377);
+      this.button1.Location = new System.Drawing.Point(352, 306);
+      this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 33);
+      this.button1.Size = new System.Drawing.Size(56, 27);
       this.button1.TabIndex = 5;
       this.button1.Text = "Расчет";
       this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(680, 450);
+      this.ClientSize = new System.Drawing.Size(510, 366);
       this.Controls.Add(this.button1);
       this.Controls.Add(this.groupBox1);
       this.Controls.Add(this.textBox2);
       this.Controls.Add(this.textBox1);
       this.Controls.Add(this.label1);
+      this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.Name = "Form1";
       this.Text = "Алгоритм с использованием циклов";
       this.groupBox1.ResumeLayout(false);
--- IT/practic2/2/FormsApp2_2/FormsApp2_2/Form1.cs
+++ IT/practic2/2/FormsApp2_2/FormsApp2_2/Form1.cs
@@ -17,7 +17,35 @@ namespace FormsApp2_2
       InitializeComponent();
     }
 
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      textBox1.Text = "1";
+      textBox2.Text = "Результат работы программы";
+      textBox2.Text += Environment.NewLine + "Рассчитать значение выражения";
+    }
+
+    public double y(double n)
+    {
+       
+    }
+
+    public double s(double y)
+    {
+      double res;
+      res = y;
+      return res;
+    }
+
     private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+    private void textBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
     {
 
     }
