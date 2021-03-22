853b6216e23a5920a1c0d53c8f31560e80ef1030
student <student@C246U24>
2021-03-22 13:47:42 +0300

    prt

--- IT/practic3/2/Frm/Frm/Form1.Designer.cs
+++ IT/practic3/2/Frm/Frm/Form1.Designer.cs
@@ -28,13 +28,96 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(81, 81);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(99, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Исходный массив";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(383, 81);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(142, 13);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "Преобразованный массив";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(59, 126);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBox1.Size = new System.Drawing.Size(181, 208);
+      this.textBox1.TabIndex = 2;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(357, 126);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBox2.Size = new System.Drawing.Size(181, 208);
+      this.textBox2.TabIndex = 3;
+      this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(84, 379);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "Заполнить";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(418, 379);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 5;
+      this.button2.Text = "Рассчитать";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.ClientSize = new System.Drawing.Size(614, 450);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Одномерный массив";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
-    #endregion
-  }
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
 }
 
--- IT/practic3/2/Frm/Frm/Form1.cs
+++ IT/practic3/2/Frm/Frm/Form1.cs
@@ -16,5 +16,50 @@ namespace Frm
     {
       InitializeComponent();
     }
+
+    int[] Mas = new int[15];
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Random rand = new Random();
+      textBox1.Text = "";
+      for (int i = 0; i < 15; i++)
+      {
+        Mas[i] = rand.Next(-50, 50);
+
+
+        textBox1.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(Mas[i]) + Environment.NewLine;
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      textBox2.Text = "";
+
+      int n = 0, m = 0;
+      for (int i = 0; i < 15; i++)
+      {
+        
+        if (Mas[i] == 0)        
+          n++;       
+          
+        if (Mas[i] < 0)        
+          m++;
+
+
+
+
+        //textBox2.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(Mas[i]) + Environment.NewLine;
+        //textBox2.Text += "Mas[" + Convert.ToString(i) + "] = " + Environment.NewLine;
+        textBox2.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(Mas[i]) + Environment.NewLine;
+      }
+      
+
+    }
+
+    private void textBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
   }
 }
