f132232648194c330b10d7e8b4d3059338737e7f
student <student@C239U28>
2021-03-22 11:57:13 +0300

    prt

--- IT/practic3/2/Forms6/Forms6/Form1.Designer.cs
+++ IT/practic3/2/Forms6/Forms6/Form1.Designer.cs
@@ -54,6 +54,7 @@
       this.label2.Size = new System.Drawing.Size(142, 13);
       this.label2.TabIndex = 1;
       this.label2.Text = "Преобразованный массив";
+      this.label2.Click += new System.EventHandler(this.label2_Click);
       // 
       // button1
       // 
@@ -63,6 +64,7 @@
       this.button1.TabIndex = 2;
       this.button1.Text = "Заполнить";
       this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // button2
       // 
@@ -72,6 +74,7 @@
       this.button2.TabIndex = 3;
       this.button2.Text = "Рассчитать";
       this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
       // 
       // textBox1
       // 
@@ -81,6 +84,7 @@
       this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
       this.textBox1.Size = new System.Drawing.Size(200, 219);
       this.textBox1.TabIndex = 4;
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
       // 
       // textBox2
       // 
@@ -90,6 +94,7 @@
       this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
       this.textBox2.Size = new System.Drawing.Size(188, 219);
       this.textBox2.TabIndex = 5;
+      this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
       // 
       // Form1
       // 
--- IT/practic3/2/Forms6/Forms6/Form1.cs
+++ IT/practic3/2/Forms6/Forms6/Form1.cs
@@ -12,12 +12,52 @@ namespace Forms6
 {
   public partial class Form1 : Form
   {
+    int[] Mas = new int[15];
+
+
     public Form1()
     {
       InitializeComponent();
     }
 
+
     private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Random rand = new Random();
+      textBox1.Text = "";
+      for (int i = 0; i < 15; i++)
+      {
+        Mas[i] = rand.Next(-50, 50);
+        textBox1.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(Mas[i]) + Environment.NewLine;
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      textBox2.Text = "";
+      for (int i = 0; i < 15; i++)
+      {
+        if (Mas[i] < 0) Mas[i] = 0;
+        textBox2.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(Mas[i]) + Environment.NewLine;
+      }
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox2_TextChanged(object sender, EventArgs e)
     {
 
     }
