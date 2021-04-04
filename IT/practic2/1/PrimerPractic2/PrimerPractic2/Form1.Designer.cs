be7544f3152f83e114cd3ba8c124d7c93a2443b9
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-04-04 23:27:14 +0300

    prrt

---
+++ IT/practic2/1/PrimerPractic2/PrimerPractic2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace PrimerPractic2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(35, 57);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(148, 17);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Введите значение х: ";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(66, 168);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(238, 17);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "Результат выполнения программы";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(229, 57);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(141, 22);
+      this.textBox1.TabIndex = 2;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(38, 205);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBox2.Size = new System.Drawing.Size(332, 175);
+      this.textBox2.TabIndex = 3;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(303, 405);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(98, 23);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "Выполнить";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(451, 450);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Линейный алгоритм";
+      this.Load += new System.EventHandler(this.Form1_Load_1);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Button button1;
+  }
+}
+
---
+++ IT/practic2/1/PrimerPractic2/PrimerPractic2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerPractic2
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load_1(object sender, EventArgs e)
+    {
+      textBox1.Text = "2";
+      // Вывод строки в многострочный редактор
+      textBox2.Text = "Практическая работа №1 Иванов И.И.";
+      textBox2.Text += Environment.NewLine + "Рассчитать значение выражения y=x^2";
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // Считывание значения X
+      double x = double.Parse(textBox1.Text);
+
+      // Вывод значения X в окно
+      textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+
+      // Вычисляем арифметическое выражение
+      double y = Math.Pow(x, 2);
+
+      // Выводим результат в окно
+      textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+    }    
+  }
+}
