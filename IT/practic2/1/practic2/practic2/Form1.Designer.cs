f2cf2ec9dc2d9803f3d333b700e6267576dc6b7c
student <student@C246U26>
2021-04-05 14:58:34 +0300

    prt2

--- IT/practic2/1/practic2/practic2/Form1.Designer.cs
+++ IT/practic2/1/practic2/practic2/Form1.Designer.cs
@@ -39,42 +39,47 @@ namespace practic2
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(49, 59);
+      this.label1.Location = new System.Drawing.Point(37, 48);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(148, 17);
+      this.label1.Size = new System.Drawing.Size(113, 13);
       this.label1.TabIndex = 0;
       this.label1.Text = "Введите значение х: ";
       // 
       // label2
       // 
       this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(49, 153);
+      this.label2.Location = new System.Drawing.Point(37, 124);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
       this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(238, 17);
+      this.label2.Size = new System.Drawing.Size(186, 13);
       this.label2.TabIndex = 1;
       this.label2.Text = "Результат выполнения программы";
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(217, 59);
+      this.textBox1.Location = new System.Drawing.Point(163, 48);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(132, 22);
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
       this.textBox1.TabIndex = 2;
       // 
       // textBox2
       // 
-      this.textBox2.Location = new System.Drawing.Point(52, 192);
+      this.textBox2.Location = new System.Drawing.Point(39, 156);
+      this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.textBox2.Multiline = true;
       this.textBox2.Name = "textBox2";
       this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-      this.textBox2.Size = new System.Drawing.Size(339, 236);
+      this.textBox2.Size = new System.Drawing.Size(255, 192);
       this.textBox2.TabIndex = 3;
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(270, 475);
+      this.button1.Location = new System.Drawing.Point(202, 386);
+      this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(108, 33);
+      this.button1.Size = new System.Drawing.Size(81, 27);
       this.button1.TabIndex = 4;
       this.button1.Text = "Выполнить";
       this.button1.UseVisualStyleBackColor = true;
@@ -82,16 +87,18 @@ namespace practic2
       // 
       // Form1
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(480, 547);
+      this.ClientSize = new System.Drawing.Size(360, 444);
       this.Controls.Add(this.button1);
       this.Controls.Add(this.textBox2);
       this.Controls.Add(this.textBox1);
       this.Controls.Add(this.label2);
       this.Controls.Add(this.label1);
+      this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
       this.Name = "Form1";
       this.Text = "Линейный алгоритм";
+      this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
       this.PerformLayout();
 
--- IT/practic2/1/practic2/practic2/Form1.cs
+++ IT/practic2/1/practic2/practic2/Form1.cs
@@ -16,10 +16,28 @@ namespace practic2
     {
       InitializeComponent();
     }
-
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      textBox1.Text = "2";
+      
+      textBox2.Text = "Практическая работа";
+      textBox2.Text += Environment.NewLine + "Рассчитать значение выражения";
+    }
     private void button1_Click(object sender, EventArgs e)
     {
+      // Считывание значения X
+      double x = double.Parse(textBox1.Text);
 
+      // Вывод значения X в окно
+      textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+
+      // Вычисляем арифметическое выражение      
+      double y = ((Math.Sqrt(1 + Math.Exp(Math.Sqrt(x)) + Math.Cos(Math.Pow(x, 2)))) / (Math.Abs(1 - Math.Pow(Math.Sin(x), 3)))) + Math.Log(2 * x);
+
+      // Выводим результат в окно
+      textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
     }
+    
+
   }
 }
