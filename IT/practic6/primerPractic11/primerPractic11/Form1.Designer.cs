d214a8ca632b7bf71eacfd31680655c7ad2dee82
student <student@C246U10>
2021-04-19 13:25:42 +0300

    prtprt11

--- IT/practic6/primerPractic11/primerPractic11/Form1.Designer.cs
+++ IT/practic6/primerPractic11/primerPractic11/Form1.Designer.cs
@@ -3,14 +3,14 @@
   partial class Form1
   {
     /// <summary>
-    ///  Required designer variable.
+    /// Обязательная переменная конструктора.
     /// </summary>
     private System.ComponentModel.IContainer components = null;
 
     /// <summary>
-    ///  Clean up any resources being used.
+    /// Освободить все используемые ресурсы.
     /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
     protected override void Dispose(bool disposing)
     {
       if (disposing && (components != null))
@@ -20,21 +20,56 @@
       base.Dispose(disposing);
     }
 
-    #region Windows Form Designer generated code
+    #region Код, автоматически созданный конструктором форм Windows
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+      this.button1.Location = new System.Drawing.Point(62, 402);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Открыть";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(622, 402);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Сохранить";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Первый графический редактор";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+
     }
 
-    #endregion
-  }
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
 }
 
--- IT/practic6/primerPractic11/primerPractic11/Form1.cs
+++ IT/practic6/primerPractic11/primerPractic11/Form1.cs
@@ -12,10 +12,117 @@ namespace primerPractic11
 {
   public partial class Form1 : Form
   {
+    //Объявляем переменные доступные в каждом обработчике события
+    private Point PreviousPoint, point; //Точка до перемещения курсора мыши и текущая точка
+    private Bitmap bmp;
+    private Pen blackPen;
+    private Graphics g;
+
+
     public Form1()
     {
       InitializeComponent();
     }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      //открытиефайла
+      OpenFileDialog dialog = new OpenFileDialog(); //описываемипорождаемобъект dialog классаOpenFileDialog
+                                                   //задаемрасширенияфайлов
+      dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+
+      if (dialog.ShowDialog() == DialogResult.OK)//вызываем диалоговое окно и проверяем выбран ли файл
+      {
+        Image image = Image.FromFile(dialog.FileName); //Загружаем в image изображение из выбранного файла
+        int width = image.Width;
+        int height = image.Height;
+        pictureBox1.Width = width;
+        pictureBox1.Height = height;
+
+        bmp = new Bitmap(image, width, height); //создаемизагружаемиз image изображениевформате bmp
+
+        pictureBox1.Image = bmp; //записываем изображение в формате bmp в pictureBox1
+        g = Graphics.FromImage(pictureBox1.Image); //подготавливаем объект Graphics для рисования в pictureBox1
+
+      }
+    }
+
+    private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+    { // обработчик события нажатия кнопки на мыши
+      // записываем в предыдущую точку (PreviousPoint) текущие координаты
+      PreviousPoint.X = e.X;
+      PreviousPoint.Y = e.Y;
+    }
+
+    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+    {//Обработчик события перемещения мыши по pictuteBox1
+      if (e.Button == MouseButtons.Left) //Проверяем нажата ли левая кнопка мыши
+      {  //запоминаем в point текущее положение курсора мыши
+        point.X = e.X;
+        point.Y = e.Y;
+
+        //соеденяем линией предыдущую точку с текущей
+        g.DrawLine(blackPen, PreviousPoint, point);
+
+        //текущее положение курсора мыши сохраняем в PreviousPoint
+        PreviousPoint.X = point.X;
+        PreviousPoint.Y = point.Y;
+        pictureBox1.Invalidate();//Принудительновызываемпереррисовку pictureBox1
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      //сохранениефайла
+      SaveFileDialog savedialog = new SaveFileDialog();//описываемипорождаемобъектsavedialog
+                                                     //задаем свойства для savedialog
+      savedialog.Title = "Сохранить картинку как ...";
+      savedialog.OverwritePrompt = true;
+      savedialog.CheckPathExists = true;
+      savedialog.Filter =
+      "Bitmap File(*.bmp)|*.bmp|" +
+      "GIF File(*.gif)|*.gif|" +
+      "JPEG File(*.jpg)|*.jpg|" +
+      "TIF File(*.tif)|*.tif|" +
+      "PNG File(*.png)|*.png";
+      savedialog.ShowHelp = true;
+      // If selected, save
+      if (savedialog.ShowDialog() == DialogResult.OK)//вызываемдиалоговоеокноипроверяемзаданолиимяфайла
+      {
+        // в строку fileName записываем указанный в savedialog полный путь к файлу
+        string fileName = savedialog.FileName;
+        // Убираем из имени три последних символа (расширение файла)
+        string strFilExtn =
+        fileName.Remove(0, fileName.Length - 3);
+        // Сохраняем файл в нужном формате и с нужным расширением
+        switch (strFilExtn)
+        {
+          case "bmp":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+            break;
+          case "jpg":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            break;
+          case "gif":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
+            break;
+          case "tif":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+            break;
+          case "png":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+            break;
+          default:
+            break;
+        }
+      }
+    }    
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      blackPen = new Pen(Color.Black, 4); //подготавливаем перо для рисования
+    }
+
 
   }
 }
