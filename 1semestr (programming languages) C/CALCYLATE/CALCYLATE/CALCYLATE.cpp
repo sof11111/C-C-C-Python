c04dc498ec9773ca15d91c5ab78359c6b10f73dc
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-11-11 17:30:45 +0300

    c

---
+++ 1semestr (programming languages) C/CALCYLATE/CALCYLATE/CALCYLATE.cpp
@@ -0,0 +1,43 @@
+﻿//CALCYLATE
+
+#include <iostream>
+#include <stdio.h>
+//#pragma warning(disable:4996)
+
+using namespace std;
+
+int main()
+{
+
+	float x1, x2;
+	char c;
+	//scanf("%f %c %f", &x1, &c, &x2);
+	cin >> x1 >> c >> x2;
+	switch (c)
+	{
+	case '+':
+		//printf("\n%f", x1 + x2);
+		cout << x1 + x2 << endl;
+		break;
+	case '-':
+		//printf("\n%f", x1 - x2);
+		cout << x1 - x2 << endl;
+		break;
+	case '*':
+		//printf("\n%f", x1 * x2); 
+		cout << x1 * x2;
+		break;
+	case '/':
+		if (x2 == 0)
+			//printf("Error davilizion by 0");
+			cout << "Error davilizion by 0";
+		else
+			//printf("\n%f", (x1) / (x2));
+			cout << (x1) / (x2) << endl;
+		break;
+	default:
+		//printf("\n operation error");
+		cout << "operation error";
+	}
+	return 0;
+}
---
+++ 1semestr (programming languages) C/adressa/adressa/adressa.cpp
@@ -0,0 +1,14 @@
+﻿//adressa +
+
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+	int A[5], i;
+	for (i = 0; i < 5; i++)
+		*(A + i) = i;//A[i]=i
+	for (i = 0; i < 5; i++)
+		printf("\n %d %d %d %d", &A[i], A[i], A + i, *(A + i));
+
+	return 0;
+}
