333af23e67df572103e772300c7acf4b53747c72
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-11-11 15:27:03 +0300

    информатика

---
+++ 1semestr (informatics) C++/factorial1/factorial1/factorial1.cpp
@@ -0,0 +1,34 @@
+﻿//factorial через функцию +
+
+#include <iostream>
+
+using namespace std;
+
+double fact(int n); //определяем функцию
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	double x;
+	int n;
+	cout << "\nВведите число 'n'> ";
+	cin >> n;
+	double xn = fact(n); //вызываем функцию и получаем результат
+
+	cout << "\nФакториал n = " << n << " равен " << xn << "\n\n";
+
+	return 0;
+}
+
+//реализуем фунцкию
+double fact(int n)
+{
+	double res = 1; //определяем переменную, которая будет результатом
+
+	for (int i = 2; i <= n; i++)
+	{
+		res = res * i; //вычисляем степень
+	}
+
+	return res; //возвращаем результат
+}
