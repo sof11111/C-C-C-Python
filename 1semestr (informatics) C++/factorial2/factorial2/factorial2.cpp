058815419ec6ad25f80153ee4f850af25c07eeef
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-11-11 15:33:53 +0300

    factorial2

---
+++ 1semestr (informatics) C++/factorial2/factorial2/factorial2.cpp
@@ -0,0 +1,34 @@
+﻿//factorial - работает везде +
+
+#include <iostream>
+using namespace std;
+
+int factorial(int numb)
+{
+	int mnoj = 1;
+	for (int a = 1; a <= numb; a++)
+	{
+		mnoj = mnoj * a;
+	}
+	return mnoj;
+}
+
+int summa(int numb)
+{
+	int sum = 0;
+	for (int a = 0; a < numb; a++)
+	{
+		sum = sum + a;
+	}
+	return sum;
+}
+
+int main()
+{
+	int sum = summa(1);
+	//cout << sum << endl;
+	int fact = factorial(3);
+	cout << fact;
+
+	return 0;
+}
