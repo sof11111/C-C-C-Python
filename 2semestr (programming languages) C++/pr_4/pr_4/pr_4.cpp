c0423873a5a47f51472b26fa86a7f2a92b8f9baa
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-11-13 16:23:52 +0300

    pr4

---
+++ 2semestr (programming languages) C++/pr_4/pr_4/pr_4.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	FILE* f;
+	int c;
+	int digitcaunter = 0, sum = 0;
+	char buf[50];
+	if ((f = fopen("t.txt", "r")) == 0)
+		perror("t.txt");
+	else
+		while ((c = fgetc(f)) != EOF)
+		{
+			putchar(c);
+			if (c >= '0' && c <= '9')
+			{
+				digitcaunter++;
+				sum += c - '0';
+			}
+		}
+	cout << "Количество чисел: " << digitcaunter << endl << "Cумма чисел: " << sum << endl;		
+	fclose(f);
+	system("pause");
+	return 0;
+}
---
+++ 2semestr (programming languages) C++/practic3_2/practic3_2/practic3_2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+//#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "Rus");
+	srand(time(0));
+	int M;
+	cin >> M;
+	int** arr = new int* [M];
+	for (int i = 0; i < M; i++)
+	{
+		arr[i] = new int[M];
+	}
+	cout << endl;
+
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			arr[i][j] = rand() % 10;
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	for (int i = 0, j = M - 1; i < M && j >= 0; i++, j--)
+	{
+		cout << arr[i][j] << " ";
+
+	}
+	cout << endl;
+
+	int summ = 0;
+
+	for (int i = 1; i < M; i++)
+	{
+		for (int j = M - i; j <= M - 1; j++)
+		{
+			summ = arr[i][j] + summ;
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << summ;
+
+	for (int i = 0; i < M; i++)
+	{
+		delete[] arr[i];
+	}
+	delete arr;
+	//int arr2[M][M];
+	return 0;
+}
