391616254f110c82e508ad45ce95cab5dc46cd3e
sof11111 <71118835+sof11111@users.noreply.github.com>
2021-11-13 16:13:33 +0300

    pr

---
+++ 2semestr (programming languages) C++/practic3/practic3/practic3.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	setlocale(0, "Rus");
+	int t, row(0), col(0);
+
+	cout << "Введите t: " << endl;
+	cin >> t;
+
+	int* a = new int[5];
+	int* b = new int[5];
+
+	cout << "заполните массив A: " << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "a[" << i << "]=";
+		cin >> a[i];
+	}
+
+
+	cout << "заполните массив B: " << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "b[" << i << "]=";
+		cin >> b[i];
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (a[i] < t)
+			row++;
+		if (b[i] < t)
+			col++;
+	}
+	cout << "В массиве А: " << row << " количество элементов, меньших значения t " << t << endl;
+
+	cout << "В массиве B: " << col << " количество элементов, меньших значения t " << t << endl;
+
+	if (row < col)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			cout << a[i] << " ";
+		}
+		cout << endl;
+
+		for (int i = 0; i < 5; i++)
+		{
+			cout << b[i] << " ";
+		}
+		cout << endl;
+	}
+
+	else
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			cout << b[i] << " ";
+		}
+		cout << endl;
+
+		for (int i = 0; i < 5; i++)
+		{
+			cout << a[i] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+
+}
